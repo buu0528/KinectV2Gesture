0d1323a9a3838423690afac238802292df2d8912
Buu <buu0528@gmail.com>
2014-11-24 19:31:51 +0900

    Initial commit.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Kinect;
+using Microsoft.Kinect.VisualGestureBuilder;
+
+namespace KinectV2Gesture
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Kinect (MultiFrame)
+        private KinectSensor kinect;
+        private MultiSourceFrameReader multiFrameReader;
+
+        // Color
+        private byte[] colorBuffer;
+        private WriteableBitmap colorImage;
+        private FrameDescription colorFrameDescription;
+
+        // Body
+        private Body[] bodies;
+
+        // Gesture Builder
+        private VisualGestureBuilderDatabase gestureDatabase;
+        private VisualGestureBuilderFrameSource gestureFrameSource;
+        private VisualGestureBuilderFrameReader gestureFrameReader;
+
+        // Gestures
+        private Gesture jump;
+        private Gesture jumpProgress;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            Closing += MainWindow_Closing;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Kinectへの接続
+            try
+            {
+                kinect = KinectSensor.GetDefault();
+                if (kinect == null)
+                {
+                    throw new Exception("Cannot open kinect v2 sensor.");
+                }
+
+                kinect.Open();
+
+                // 初期設定
+                Initialize();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                Close();
+            }
+        }
+
+        void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (multiFrameReader != null)
+            {
+                multiFrameReader.Dispose();
+                multiFrameReader = null;
+            }
+            if (gestureFrameReader != null)
+            {
+                gestureFrameReader.Dispose();
+                gestureFrameReader = null;
+            }
+            if (kinect != null)
+            {
+                kinect.Close();
+                kinect = null;
+            }
+        }
+
+        /// <summary>
+        /// 初期設定
+        /// </summary>
+        private void Initialize()
+        {
+            // ColorImageの初期設定
+            colorFrameDescription = kinect.ColorFrameSource.CreateFrameDescription(ColorImageFormat.Bgra);
+            colorImage = new WriteableBitmap(colorFrameDescription.Width, colorFrameDescription.Height, 96, 96, PixelFormats.Bgra32, null);
+            ImageColor.Source = colorImage;
+
+            // Bodyの初期設定
+            bodies = new Body[kinect.BodyFrameSource.BodyCount];
+
+            // Gesturesの初期設定
+            gestureDatabase = new VisualGestureBuilderDatabase(@"../../Gestures/Jump.gbd");
+            gestureFrameSource = new VisualGestureBuilderFrameSource(kinect, 0);
+
+            // 使用するジェスチャーをデータベースから取り出す
+            foreach (var gesture in gestureDatabase.AvailableGestures)
+            {
+                if (gesture.Name == "Jump")
+                {
+                    jump = gesture;
+                }
+                if (gesture.Name == "JumpProgress")
+                {
+                    jumpProgress = gesture;
+                }
+                this.gestureFrameSource.AddGesture(gesture);
+            }
+
+            // ジェスチャーリーダーを開く
+            gestureFrameReader = gestureFrameSource.OpenReader();
+            gestureFrameReader.IsPaused = true;
+            gestureFrameReader.FrameArrived += gestureFrameReader_FrameArrived;
+
+            // フレームリーダーを開く (Color / Body)
+            multiFrameReader = kinect.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Body);
+            multiFrameReader.MultiSourceFrameArrived += multiFrameReader_MultiSourceFrameArrived;
+
+        }
+
+        private void multiFrameReader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            MultiSourceFrame multiFrame = e.FrameReference.AcquireFrame();
+
+            // Colorの取得と表示
+            using (var colorFrame = multiFrame.ColorFrameReference.AcquireFrame())
+            {
+                if (colorFrame == null)
+                {
+                    return;
+                }
+
+                // RGB画像の表示
+                colorBuffer = new byte[colorFrameDescription.Width * colorFrameDescription.Height * colorFrameDescription.BytesPerPixel];
+                colorFrame.CopyConvertedFrameDataToArray(colorBuffer, ColorImageFormat.Bgra);
+
+                ImageColor.Source = BitmapSource.Create(colorFrameDescription.Width, colorFrameDescription.Height, 96, 96,
+                    PixelFormats.Bgra32, null, colorBuffer, colorFrameDescription.Width * (int)colorFrameDescription.BytesPerPixel);
+
+            }
+
+            // Bodyを１つ探し、ジェスチャー取得の対象として設定
+            if (!gestureFrameSource.IsTrackingIdValid)
+            {
+                using (BodyFrame bodyFrame = multiFrame.BodyFrameReference.AcquireFrame())
+                {
+                    if (bodyFrame != null)
+                    {
+                        bodyFrame.GetAndRefreshBodyData(bodies);
+
+                        foreach (var body in bodies)
+                        {
+                            if (body != null && body.IsTracked)
+                            {
+                                // ジェスチャー判定対象としてbodyを選択
+                                gestureFrameSource.TrackingId = body.TrackingId;
+                                // ジェスチャー判定開始
+                                gestureFrameReader.IsPaused = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void gestureFrameReader_FrameArrived(object sender, VisualGestureBuilderFrameArrivedEventArgs e)
+        {
+            using (var gestureFrame = e.FrameReference.AcquireFrame())
+            {
+                // ジェスチャーの判定結果がある場合
+                if (gestureFrame != null && gestureFrame.DiscreteGestureResults != null)
+                {
+                    var result = gestureFrame.DiscreteGestureResults[jump];
+                    if (result.Detected)
+                    {
+                        // ジャンプしている場合、その進捗をプログレスバーに反映
+                        var progressResult = gestureFrame.ContinuousGestureResults[jumpProgress];
+                        ProgressBar1.Value = progressResult.Progress;
+                    }
+                    else
+                    {
+                        ProgressBar1.Value = 0.0;
+                    }
+
+                    TextBlock1.Text = result.Detected ? "Jump!" : "No Gesture Found."; // ジャンプしているか？
+                    TextBlock2.Text = "Confidence: " + result.Confidence.ToString();   // ジャンプの確からしさ
+                }
+            }
+        }
+
+    }
+}
